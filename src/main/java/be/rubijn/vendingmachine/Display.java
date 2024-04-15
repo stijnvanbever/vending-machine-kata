bf1744cb394d6d78bd15a53071325810b82328d4
Ruben Teck <ruben.teck@cegeka.com>
2024-04-15 16:40:54 +0200

    second requirement v2.2

--- src/main/java/be/rubijn/vendingmachine/Display.java
+++ src/main/java/be/rubijn/vendingmachine/Display.java
@@ -8,11 +8,15 @@ import java.util.Stack;
 public class Display {
     Stack<String> text;
 
-    public String readText(int amount) {
-        return String.format("INSERT COIN.\nAMOUNT: %.2f", amount / 100f);
+    public String read(int amount) {
+        return String.format("%s\n%s", readText(), readAmount(amount));
+    }
+
+    public String readText(){
+        return "INSERT COIN";
     }
 
     public String readAmount(int amount){
-
+        return String.format("AMOUNT: %.2f", amount / 100f);
     }
 }
